d9b296a9d6127699ceac41b736f54f57c71daa94
ManassehPierce <manassehpierce@gmail.com>
2016-12-02 17:42:32 -0600

    fixed bugs

--- index.js
+++ index.js
@@ -1,93 +1,92 @@
-// include the Discord library
-const Discord = require("discord.js");
-// include "filesystem"
-const fs = require("fs");
-// make a new Discord Client, basiclly a user
-const client = new Discord.Client();
+var Discord = require("discord.js");
+var fs = require("fs");
+var client = new Discord.Client();
 
-// our JSON file for storing User Data
-let users = JSON.parse(fs.readFileSync('users.json', 'utf8'));
+var guilds;
 
-// when the Bot logs in
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.username}#${client.user.discriminator}`);
+    guilds = JSON.parse(fs.readFileSync('users.json', 'utf8')) ? JSON.parse(fs.readFileSync('users.json', 'utf8')) : {};
+    client.guilds.array().forEach((guild) => {
+        if(!guilds[guild.id]) {
+            guilds[guild.id] = {};
+        }
+    });
 });
 
-// when a message is sent
 client.on('message', msg => {
-    let format = msg.content.toLowerCase(); // make all the text lowercase, allows for non-case-sensitive commands
-    if(!msg.author.bot) { // make sure a bot didn't message us
-        if (format.includes('time bot') || format.includes('timebot')) { // if you reference the Time Bot
-            let date = new Date(); // get the date
-            let cmd = format.split(' '); // split the whole line of text into seperate words
+    // check if the message came from a bot, no loops!
+    if(!msg.author.bot) {
+        // check if the message contains "time bot" or "timebot"
+        var formatted = msg.content.split(' ');
+        if(formatted[0].toLowerCase() === "time" && formatted[1].toLowerCase() === "bot") {
+            formatted.reverse();
+            formatted.pop();
+            formatted.reverse();
+            formatted[0] = "timebot";
+        }
 
-            if(cmd[0] === "time" && cmd[1] === "bot") { // "time bot"
-                if(cmd[2] === "add") { // "time bot add USERNAME NUMBER"
-                    // add the user
-                    addUser(msg.guild, cmd[3], parseInt(cmd[4]));
-                    msg.reply(`Added ${cmd[3]}`);
-                } else if(cmd[2] === "remove") { // "time bot remove USERNAME"
-                    // remove the user
-                    removeUser(msg.guild, cmd[3]);
-                    msg.reply(`Removed ${cmd[3]}`);
-                } else { // "time bot ..."
-                    // send a message back with the times of everyone
-                    let times = returnTimes(msg.guild, date);
-                    if(times) {
-                        msg.reply(times.join('\n'));
-                    } else {
-                        msg.reply('There are no users entered, please use time bot add USERNAME TIMEDIFFERENCE, or timebot add USERNAME TIMEDIFFERENCE');
-                    }
-                }
-            } else if(cmd[0] === "timebot") { // "timebot"
-                if(cmd[1] === "add") { // "timebot add USERNAME NUMBER"
-                    // add the user
-                    addUser(msg.guild, cmd[2], parseInt(cmd[3]));
-                    msg.reply(`Added ${cmd[2]}`);
-                } else if(cmd[1] === "remove") { // "timebot remove USERNAME"
-                    // remove the user
-                    removeUser(msg.guild, cmd[2]);
-                    msg.reply(`Removed ${cmd[2]}`);
-                } else { // "timebot ..."
-                    // send a message back with the times of everyone
-                    let times = returnTimes(msg.guild, date);
-                    if(times) {
-                        msg.reply(times.join('\n'));
-                    } else {
-                        msg.reply('There are no users entered, please use time bot add USERNAME TIMEDIFFERENCE, or timebot add USERNAME TIMEDIFFERENCE');
-                    }
-                }
+        if(formatted[0] === "timebot") {
+            // now we can check for commands
+            if(formatted[1] === "help") {
+                msg.reply(help());
+            } else if(formatted[1] === "add") {
+                // do add
+                formatted[2] = formatted[2].replace(formatted[2][0], formatted[2][0].toUpperCase());
+                add(msg.guild, formatted[2], parseInt(formatted[3]));
+                msg.reply(`Added ${formatted[2]} with a UTC difference of ${formatted[3]}`);
+            } else if(formatted[1] === "remove") {
+                // do remove
+                formatted[2] = formatted[2].replace(formatted[2][0], formatted[2][0].toUpperCase());
+                remove(msg.guild, formatted[2]);
+                msg.reply(`Removed ${formatted[2]}`);
+            } else {
+                msg.reply('Times are:\n' + get(msg.guild));
             }
         }
     }
 });
 
-function addUser(guild, username, timediff) {
-    users[guild][username] = timediff;
-    fs.writeFileSync('users.json', JSON.stringify(users));
-}
-
-function removeUser(guild, username) {
-    users[guild][username] = null;
-    fs.writeFileSync('users.json', JSON.stringify(users));
-}
-
-function returnTimes(guild, date) {
-    let keys;
-    if(users) {
-        keys = Object.keys(users[guild]);
-    }
-    else {
-        return;
-    }
-    let temp = [
-        `Times for Users in ${guild}:`
-    ];
-    keys.forEach(function(key) {
-        if(users[guild][key] !== null) temp.push( `${key}: ${date.getHours() + users[guild][key]}:${date.getMinutes()}` );
+function get(guild) {
+    var group = guilds[guild.id];
+    var date = new Date();
+    var hour = date.getUTCHours();
+    var minutes = date.getUTCMinutes();
+    var keys = Object.keys(group);
+    var temp = [];
+    keys.forEach((k) => {
+        var val = group[k]; // -6
+        var tHour = hour + val;
+        if(tHour >= 24) {
+            tHour -= 24;
+        } else if(tHour < 0) {
+            tHour = 24 + tHour;
+        }
+        temp.push(`${k}: ${tHour}:${minutes}`);
     });
-    return temp;
+    return temp.join('\n');
 }
 
-// the token for my bot
+function add(guild, username, utcdiff) {
+    guilds[guild.id][username] = utcdiff;
+    fs.writeFileSync('users.json', JSON.stringify(guilds));
+}
+
+function remove(guild, username) {
+    if(guilds[guild.id][username]) {
+        guilds[guild.id][username] = null;
+    }
+    fs.writeFileSync('users.json', JSON.stringify(guilds));
+}
+
+function help() {
+    return [
+        'To activate time bot, type \'time bot\' or \'timebot\'',
+        'To display help, type \'timebot help\'',
+        'To add users to the time bot list, type \'timebot add Username TimeDifferenceFromUTC\'',
+        'To edit a users time difference, just \"add\" them again',
+        'To remove a user from the time bot list, type \'timebot remove Username\''
+    ].join('\n');
+}
+
 client.login('MjU0MjI2ODkzNjI3MzI2NDY1.CyL_AA.zsC8DSmr12BQFxD3NVcunDj1T0k');
